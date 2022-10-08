599146c2369bca282c4861e0f6cb2f6d3013c83d
PiotrWesoly <Piotrpwesoly@gmail.com>
2022-10-08 18:12:01 +0200

    Update code.js

--- code.js
+++ code.js
@@ -17,6 +17,18 @@ function colorBorder(inputName) {
 }
 
 function isValid(inputName){
+    let input = document.getElementById(inputName);
+
+    if(inputName == 'fmial')
+    {
+        return input.match(
+            /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          );
+    }
+    else if(inputName == 'lpost')
+    {
+        return input.match();
+    }
     return false;
 
 }
