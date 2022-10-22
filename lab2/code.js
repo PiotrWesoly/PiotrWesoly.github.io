d2959e01e7d5c28c0e2164b372bb87e61f769524
PiotrWesoly <Piotrpwesoly@gmail.com>
2022-10-22 19:00:46 +0200

    Add fill for time24

--- lab2/code.js
+++ lab2/code.js
@@ -11,5 +11,7 @@ function fillOutRandom() {
     document.getElementById("ip6").value = 'FE80:0000:0000:0000:3423:B3FF:F8FF:1231';
     document.getElementById("phone").value = '+48798293493';
     document.querySelector('input[type="time"]').value = '11:30';
+    document.getElementById("time24").value = '11:40';
+
 
 }
