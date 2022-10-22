fd67f758966c0f11958bc56fe4d2b8a0c1ae36e1
PiotrWesoly <Piotrpwesoly@gmail.com>
2022-10-22 18:19:36 +0200

    Lab 2 init

---
+++ lab2/code.js
@@ -0,0 +1,14 @@
+function fillOutRandom() {
+    document.getElementById("fmail").value = "piotr@gmail.com";
+    document.getElementById("postal").value = "90-111";
+    document.getElementById("nip").value = "111-222-33-44";
+    document.getElementById("ID").value = "DBZ843920";
+    document.getElementById("IP4").value = "1.3.2.4";
+    document.getElementById("web").value = "www.asdasd.com";
+    document.getElementById("dir").value = "C:\\windows\\temp";
+    document.getElementById("directory_big").value = 'C:\\user\\desktop\\Lab';
+    document.getElementById("file").value = '/etc/hosts';
+    document.getElementById("ip6").value = 'FE80:0000:0000:0000:3423:B3FF:F8FF:1231';
+    document.getElementById("phone").value = '+48798293493';
+    
+}
