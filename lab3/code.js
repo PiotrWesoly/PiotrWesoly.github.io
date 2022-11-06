a7071e5ea38643940c17133d46461f03010b1762
PiotrWesoly <Piotrpwesoly@gmail.com>
2022-11-06 12:36:08 +0100

    Finished implementation

--- lab3/code.js
+++ lab3/code.js
@@ -12,7 +12,10 @@ let dropIndex;
       else if (target.className == "input") 
       {
 		  element = target.parentNode;
-	  }
+	  }else
+      {
+        return;
+      }
 
       elementId = element.id;
       elementList = element.parentNode.children;
@@ -32,35 +35,60 @@ let dropIndex;
 
   document.addEventListener("drop", ({target}) => {
    let draggedElement;
-   if(target.className == "element") 
+
+   if(target.className == "element" || target.className == "input")
    {
-	   draggedElement = target;
-   } 
-   else if (target.className == "input") 
-   {
-	  draggedElement = target.parentNode;
-   } 
-   else 
-   {
-	   return;
-   }
-   
-   if(draggedElement.id !== elementId) 
-   {
-	  element.remove(element);
-	  for(let i = 0; i < elementList.length; i++) {
-		if(elementList[i] === draggedElement)
+        if(target.className == "element") 
         {
-		  dropIndex = i;
-		}
-	  }
-	  if(elementIndex > dropIndex) 
-      {
-		draggedElement.before(element);
-		return;
-	  } 
-	  draggedElement.after(element);
-	 }
+            draggedElement = target;
+        } 
+        else if (target.className == "input") 
+        {
+            draggedElement = target.parentNode;
+        } 
+        else 
+        {
+            return;
+        }
+        
+        if(draggedElement.id !== elementId) 
+        {
+            element.remove(element);
+            for(let i = 0; i < elementList.length; i++) {
+                if(elementList[i] === draggedElement)
+                {
+                dropIndex = i;
+                }
+            }
+            if(elementIndex > dropIndex) 
+            {
+                draggedElement.before(element);
+                return;
+            } 
+            draggedElement.after(element);
+            }
+    }
+
+     if(target.className == "cell") {
+        let parsedId = parseId(target.id);
+
+        console.log(parsedId);
+        let yCoordinate = parsedId[0];
+        console.log(array);
+        while (yCoordinate + 1 < height) {
+            yCoordinate++;
+            console.log(yCoordinate);
+            if (array[yCoordinate][parsedId[1]] == true) {
+                let cell = document.getElementById((yCoordinate - 1) + "_" + parsedId[1]);
+                cell.style.background = color;
+                array[yCoordinate - 1][parsedId[1]] = true;
+                return;
+            }
+        }
+        let cell = document.getElementById(height - 1 + "_" + parsedId[1]);
+        cell.style.background = color;
+        array[height - 1][parsedId[1]] = true;
+        } 
   });
 
 let color;
@@ -72,10 +100,42 @@ function setRandomColor() {
   }
 
 function getRandomColor() {
-var letters = '0123456789ABCDEF';
-var color = '#';
-for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
-return color;
+
+let width = 8;
+let height = 4;
+
+function init(event)
+{
+    let platform = document.getElementById('platform');
+	array = new Array(height);
+
+    console.log("INIT");
+
+	for (var i = 0; i < height; i++) {
+        array[i] = new Array(width);
+        let newTr = document.createElement("tr");
+        platform.appendChild(newTr);
+        for (var j = 0; j < width; j++) {
+            array[i][j] = false;
+            let newTd = document.createElement("td");
+            newTd.id = i + "_" + j;
+            newTd.className = "cell";
+            // newTd.textContent="CELL"+i+"-"+j;
+            newTr.appendChild(newTd);
+        }
+    }
 }
+
+function parseId(id) {
+	let parsedId = id.split("_");
+	parsedId[0] = parseInt(parsedId[0]);
+	parsedId[1] = parseInt(parsedId[1]);
+	return parsedId;
+  }
