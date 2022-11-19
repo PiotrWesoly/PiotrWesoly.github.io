ab3aae1873ac3d3ac76ac62df10cc35f29d6692f
PiotrWesoly <Piotrpwesoly@gmail.com>
2022-11-19 18:40:17 +0100

    create form, add initial implementation for DB

---
+++ lab4/code.js
@@ -0,0 +1,34 @@
+let db;
+const request = window.indexedDB.open("MyDatabase", 3);
+
+request.onerror = (event) => {
+  console.error("Why didn't you allow my web app to use IndexedDB?!");
+};
+
+request.onsuccess = (event) => {
+  db = event.target.result;
+};
+
+db.onerror = (event) => {
+    // Generic error handler for all errors targeted at this database's
+    // requests!
+    console.error(`Database error: ${event.target.errorCode}`);
+  };
+
+request.onupgradeneeded = function (event) {
+var db = event.target.result;
+var objectStore = db.createObjectStore("client", {
+    autoIncrement: true,
+});
+
+objectStore.createIndex("name", "name", { unique: false });
+objectStore.createIndex("lastName", "lastName", { unique: false });
+objectStore.createIndex("email", "email", { unique: true });
+objectStore.createIndex("ID", "ID", { unique: true });
+objectStore.createIndex("postal", "postal", { unique: false });
+objectStore.createIndex("phoneNumber", "phoneNumber", { unique: true });
+
+for (var i in clientData) {
+    objectStore.add(clientData[i]);
+}
+};
