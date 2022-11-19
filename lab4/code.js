adad8a7e15db12c1b908fb433955001030114168
PiotrWesoly <Piotrpwesoly@gmail.com>
2022-11-19 19:40:59 +0100

    add table and remove option

--- lab4/code.js
+++ lab4/code.js
@@ -1,19 +1,15 @@
 let db;
-const request = window.indexedDB.open("MyDatabase", 3);
+let request = window.indexedDB.open("newDatabase", 1);
 
-request.onerror = (event) => {
-  console.error("Why didn't you allow my web app to use IndexedDB?!");
+request.onerror = function (event) {
+console.log("error: The database is opened failed");
 };
 
-request.onsuccess = (event) => {
-  db = event.target.result;
+request.onsuccess = function (event) {
+db = request.result;
+console.log("success: The database " + db + " is opened successfully");
+drawTable();
 };
-
-db.onerror = (event) => {
-    // Generic error handler for all errors targeted at this database's
-    // requests!
-    console.error(`Database error: ${event.target.errorCode}`);
-  };
 
 request.onupgradeneeded = function (event) {
 var db = event.target.result;
@@ -39,7 +35,7 @@ const clientData = [
       lastName: "Wesoly",
       email: "PiotrWesoly@gmail.com",
       ID: 'CCU238293',
-      addres: "Kilinskiego 12",
+      postal: "90-234",
       phoneNumber: "500500200"
     },
     {
@@ -47,7 +43,7 @@ const clientData = [
       lastName: "Rosiak",
       email: "pawelRosiak@gmail.com",
       ID: 'CCU238293',
-      address: "Piotrkowska 12",
+      postal: "93-234",
       phoneNumber: "500400200"
     },
   ];
@@ -72,14 +68,15 @@ function add(event) {
       .add({
         name: formElements[0].value,
         lastName: formElements[1].value,
-        email: formElements[3].value,
-        postal: formElements[4].value,
-        ID: formElements[5].value,
-        phoneNumber: formElements[6].value,
+        email: formElements[2].value,
+        postal: formElements[3].value,
+        ID: formElements[4].value,
+        phoneNumber: formElements[5].value,
       });
 
     request.onsuccess = function (event) {
       console.log("Client added");
+      drawTable();
     };
 
     request.onerror = function (event) {
@@ -88,3 +85,97 @@ function add(event) {
       );
     };
   }
+
+  function drawTable(filterItems) {
+    if (document.getElementById("tbody") !== null) {
+      document.querySelector("#tbody").remove();
+    }
+
+    let table = document.createElement("table");
+    table.setAttribute("id", "tbody");
+    let data = Object.keys(clientData[0]);
+    generateTable(table, filterItems);
+    generateTableHead(table, data);
+    document.getElementById("tableDiv").appendChild(table);
+  }
+
+  function generateTableHead(table, data) {
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+
+    // Create id column
+    let th = document.createElement("th");
+    let text = document.createTextNode("id");
+    th.appendChild(text);
+    row.appendChild(th);
+
+    for (let key of data) {
+      let th = document.createElement("th");
+      let text = document.createTextNode(key);
+      th.appendChild(text);
+      row.appendChild(th);
+    }
+  }
+
+  function generateTable(table, filterItems = []) {
+    let objectStore = db.transaction("client").objectStore("client");
+
+    objectStore.openCursor().onsuccess = function (event) {
+      var cursor = event.target.result;
+
+      if (cursor) {
+        console.log(filterItems);
+        if (filterItems.length > 0 && filterItems[0] !== "") {
+          let exists = false;
+          for (let i = 0; i < filterItems.length; i++) {
+            const element = filterItems[i];
+            
+            if (Object.values(cursor.value).includes(element)) {
+              exists = true
+            }
+          }
+
+          if (!exists) {
+            cursor.continue();
+            return;
+          }
+        }
+
+        console.log(cursor.value)
+
+        let row = table.insertRow();
+        let cell = row.insertCell();
+        let text = document.createTextNode(cursor.key);
+        cell.appendChild(text);
+        for (const [key, value] of Object.entries(cursor.value)) {
+          let cell = row.insertCell();
+          let text = document.createTextNode(value);
+          cell.appendChild(text);
+        }
+
+        cell = row.insertCell();
+        let removeButton = document.createElement("button");
+        removeButton.setAttribute("id", "removeButton" + cursor.key);
+        removeButton.setAttribute("onclick", `remove(${cursor.key})`);
+        removeButton.innerHTML = "remove";
+        cell.appendChild(removeButton);
+        cursor.continue();
+
+      } else {
+        console.log("No more data");
+      }
+    };
+  }
+
+  function remove(id) {
+    let request = db
+      .transaction(["client"], "readwrite")
+      .objectStore("client")
+      .delete(id);
+
+    request.onsuccess = function (event) {
+      console.log(`Client ${id} removed...`);
+      drawTable();
+    };
+  }
+  
