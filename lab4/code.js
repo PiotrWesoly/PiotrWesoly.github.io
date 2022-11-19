9bfcc04e9171cf3527bbac50e60a26d63d7a3b7f
PiotrWesoly <Piotrpwesoly@gmail.com>
2022-11-19 20:47:23 +0100

    update

--- lab4/code.js
+++ lab4/code.js
@@ -1,5 +1,26 @@
+window.indexedDB =
+        window.indexedDB ||
+        window.mozIndexedDB ||
+        window.webkitIndexedDB ||
+        window.msIndexedDB;
+
+      //prefixes of window.IDB objects
+      window.IDBTransaction =
+        window.IDBTransaction ||
+        window.webkitIDBTransaction ||
+        window.msIDBTransaction;
+      window.IDBKeyRange =
+        window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+
+      if (!window.indexedDB) {
+        window.alert(
+          "Your browser doesn't support a stable version of IndexedDB."
+        );
+      }
+
+
 let db;
-let request = window.indexedDB.open("newDatabase", 1);
+let request = window.indexedDB.open("newDatabase", 2);
 
 
 request.onerror = function (event) {
