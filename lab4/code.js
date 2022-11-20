183c4f9bd5fabff192dde18f89e37fb9ba858e81
PiotrWesoly <Piotrpwesoly@gmail.com>
2022-11-20 13:59:02 +0100

    change version to 2

--- lab4/code.js
+++ lab4/code.js
@@ -30,7 +30,7 @@ window.indexedDB =
 // };
 
 let db;
-let request = window.indexedDB.open("newDatabase", 1);
+let request = window.indexedDB.open("newDatabase222", 2);
 
 request.onerror = function (event) {
 console.log("error: The database is opened failed");
